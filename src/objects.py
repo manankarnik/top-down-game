877443e3d5326e223677131fcaf0f0313a1f48f5
Manan Karnik <manankarnik03@gmail.com>
2022-04-11 17:59:30 +0530

    Fixed dash indicator and added states

--- src/objects.py
+++ src/objects.py
@@ -3,6 +3,17 @@ import pygame
 from config import *
 
 
+class Sprite(pygame.sprite.Sprite):
+    def __init__(self, image: pygame.surface.Surface, position: pygame.Vector2, group=None) -> None:
+        if group is None:
+            super().__init__()
+        else:
+            super().__init__(group)
+
+        self.image = image
+        self.rect = self.image.get_rect(center=position)
+
+
 class Object(pygame.sprite.Sprite):
     def __init__(self, position: pygame.Vector2, group: pygame.sprite.Group) -> None:
         super().__init__(group)
